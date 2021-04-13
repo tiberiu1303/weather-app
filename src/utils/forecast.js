e10dbf9bada8b200fb85863e03656a3749bb7717
Tiberiu Stancu <tiberiu.stancu@ro.softvision.global>
2021-04-13 18:00:38 +0300

    Adding humidity info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,6 +3,7 @@ const apiSecretKey = '0482842f0fc8f17eb705d4e2afee9b10';
 
 module.exports = forecast = (longitude, latitude, callback) => {
     let url = `http://api.weatherstack.com/current?access_key=${apiSecretKey}&query=${latitude},${longitude}`;
+    console.log(url);
     request({url, json: true}, (error, response, body) => {
         if (error) {
             callback('Unable to connect to location services', undefined);
@@ -11,7 +12,7 @@ module.exports = forecast = (longitude, latitude, callback) => {
         } else {
             const currentData = body.current;
 
-            callback(undefined, `It is ${currentData.weather_descriptions[0]}, currently ${currentData.temperature} degrees out and it feels like ${currentData.feelslike} degrees out.`);
+            callback(undefined, `It is ${currentData.weather_descriptions[0]}, currently ${currentData.temperature} degrees out and it feels like ${currentData.feelslike} degrees out. The huminidy is ${currentData.humidity}%`);
         }
     })
 };
